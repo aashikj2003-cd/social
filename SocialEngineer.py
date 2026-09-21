0df22576c82df5640271960d84ceaa0103980d52
aashikj2003-cd <aashikj2003@gmail.com>
2026-09-21 16:00:38 +0530

    Update SocialEngineer.py

--- SocialEngineer.py
+++ SocialEngineer.py
@@ -4,8 +4,8 @@
 SocialEngineer - Social Engineering Toolkit
 -------------------------------------------
 
-Author      : Karthikeyan (https://karthithehacker.com)
-GitHub      : https://github.com/karthi-the-hacker
+Author      : TEAM PPS
+GitHub      : 
 Project     : SocialEngineer - An all-in-one CLI framework for social engineering
 
 License     : Open-source — strictly for educational and ethical hacking purposes ONLY.
